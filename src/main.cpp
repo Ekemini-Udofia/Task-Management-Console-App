81396ae9b5a07b9934870245de9afae069ffa6db
ekemini_udofia <ekeminiabasiudofia@gmail.com>
2025-05-26 15:23:36 +0100

    Implemented persistent storage for the user credentials(only the name sha)

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include "lib/Task.h"
 #include <fstream>
-#include <filesystem>
+//#include <filesystem>
 
 
 
@@ -13,22 +13,23 @@
 // Create the general task vectors for storing the tasks
 std::vector<Task> Task_List;
 
-std::string global_username = "User"; // Default username
+std::string global_username = ""; // Default username
 
-// Create an output file for writing to 
-
-std::ofstream new_file("task_data.txt");
 
 
 static void greet_user()
 {
-    int i = 0;
-    if (i == 0)
+    if (global_username == "")
     {
-        //std::cin.ignore();
         std::cout << "> Welcome to your Personal Task Management app!\n> Let's get to know you.\n> Name: ";
         std::getline(std::cin, global_username);
         std::cout << "> Nice to meet you, " << global_username << "!\n";
+        std::ofstream new_file("cred.txt");
+        if (new_file.is_open()) {
+            new_file << global_username << std::endl;
+            new_file.close();
+            std::cout << "> Username Saved" << std::endl;
+        }
     }
     else
     {
@@ -104,7 +105,14 @@ static void display_options()
     else if (option == 5)
     {
         std::cout << "Input a new username: ";
-        std::cin >> global_username;
+        std::cin.ignore();
+        std::getline(std::cin, global_username);
+        std::ofstream inFile("cred.txt", std::ios::trunc); 
+        if (inFile.is_open()) {
+            inFile << global_username << std::endl;
+            inFile.close();
+            std::cout << "> Username Saved" << std::endl;
+        }
         display_options();
     }
     else if (option == 6)
@@ -122,9 +130,23 @@ static void display_options()
 
 int main(int argc, char* argv[])
 {
-    greet_user();
+
+    // Initializations
+    // Create an output file for writing to 
+    // check if theres a file like that named already
+    
+    //write user name to it
+    std::ifstream inFile("cred.txt");
+    if (!inFile.is_open())
+    {
+        greet_user();
+    }
+    else
+    {
+        std::getline(inFile, global_username);
+        inFile.close();
+        greet_user();
+    }
     display_options();
-    //Task global_task_name;
-    //std::cout << global_task_name << "\n";
     return 0;
 }
